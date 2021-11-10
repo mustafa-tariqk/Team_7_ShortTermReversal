772ae21d76ea958c70ecb62fd2fc88a47ed7baf7
Mustafa Tariq <mustafa-tariqk@pm.me>
2021-11-10 18:42:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+class JumpingBlackCamel(QCAlgorithm):
 
+    def Initialize(self):
+        self.SetStartDate(2021, 5, 9)  # Set Start Date
+        self.SetCash(100000)  # Set Strategy Cash
+        # self.AddEquity("SPY", Resolution.Minute)
+
+
+    def OnData(self, data):
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+            Arguments:
+                data: Slice object keyed by symbol containing the stock data
+        '''
+
+        # if not self.Portfolio.Invested:
+        #    self.SetHoldings("SPY", 1)
