f5c3af901b2cb567dde660fb5a6f8f282645c14a
Mustafa Tariq <mustafa@tariqk.tech>
2021-11-24 13:55:29 -0500

    course selection added

--- main.py
+++ main.py
@@ -6,8 +6,34 @@ class Team7Algo(QCAlgorithm):
     def OnSecuritiesChanged(self, changes):
         pass
         
-    def CoarseSelection(self, coarse):
-        pass
+    def CoarseSelectionFunction(self, coarse):
+        for stock in coarse:
+            symbol = stock.Symbol
+
+            if symbol in self.data:
+                self.data[symbol].update(stock.AdjustedPrice)
+
+        if not self.selection_flag:
+            return Universe.Unchanged
+
+        selected = sorted([x for x in coarse if x.HasFundamentalData and x.Market == 'usa' and x.Price > 1],
+            key=lambda x: x.DollarVolume, reverse=True)
+        selected = [x.Symbol for x in selected][:self.coarse_count]
+
+        for symbol in selected:
+            if symbol in self.data:
+                continue
+
+            self.data[symbol] = SymbolData(self.period)
+            history = self.History(symbol, self.period, Resolution.Daily)
+            if history.empty:
+                self.Log(f"Not enough data for {symbol} yet")
+                continue
+            closes = history.loc[symbol].close
+            for time, close in closes.iteritems():
+                self.data[symbol].update(close)
+
+        return [x for x in selected if self.data[x].is_ready()]
 
     def FineSelection(self, fine):
         pass
